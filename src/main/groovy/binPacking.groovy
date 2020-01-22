a85e582086994b4cd8fd81fdb83414329e8a171f
maorgo <maor.goaz@logz.io>
2020-01-22 15:29:34 +0200

    Style changes

--- src/main/groovy/binPacking.groovy
+++ src/main/groovy/binPacking.groovy
@@ -1,5 +1,6 @@
 class binPacking {
     static void main(String[] args) {
+        def bucketCapacity = 15
         Map classesToRunningTime = [
                 "class1": 5,
                 "class2": 1,
@@ -12,6 +13,25 @@ class binPacking {
                 "class9": 0
         ]
 
+        List<TestClass> classesToAttach = getSortedTestClasses(classesToRunningTime)
+        List<Bucket> buckets = firstFit(classesToAttach, bucketCapacity)
+        printBucketsResult(buckets)
+
+
+    }
+
+    private static void printBucketsResult(List<Bucket> buckets) {
+        println("Total buckets: " + buckets.size())
+        for (Bucket bucket : buckets) {
+            println("**** New bucket run time: " + bucket.totalRunTime)
+            println(bucket.attachedClasses.size() + " classes")
+            for (String className : bucket.attachedClasses) {
+                println(className)
+            }
+        }
+    }
+
+    private static List<TestClass> getSortedTestClasses(LinkedHashMap<String, Integer> classesToRunningTime) {
         List<TestClass> classesToAttach = []
 
         for (entry in classesToRunningTime) {
@@ -19,21 +39,8 @@ class binPacking {
             classesToAttach.add(testClass)
         }
 
-        List<TestClass> sortedTestClassList = classesToAttach.sort { -it.runTime }
-
-        def bucketCapacity = 15
-        List<Bucket> buckets = firstFit(sortedTestClassList, bucketCapacity)
-
-        println("Total buckets: " + buckets.size())
-        for (Bucket bucket: buckets) {
-            println("**** New bucket run time: " + bucket.totalRunTime)
-            println(bucket.attachedClasses.size() + " classes")
-            for (String className: bucket.attachedClasses) {
-                println(className)
-            }
-        }
-
-
+        classesToAttach.sort { -it.runTime }
+        classesToAttach
     }
 
     static def firstFit(List<TestClass> classesToAttach, int bucketCapacity) {
@@ -43,8 +50,6 @@ class binPacking {
             int j
             for (j = 0; j < bucketList.size(); j++) {
                 if (bucketList[j].remainingBinSpace >= classesToAttach[i].runTime) {
-//                    double remainingBinSpace = bucketList[j].remainingBinSpace - classesToAttach[i].runTime
-//                    bucketList[j].setRemainingBinSpace(remainingBinSpace)
                     bucketList[j].add(classesToAttach[i])
                     break
                 }
